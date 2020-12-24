b0e2776a43a6b156d9e9ec1664372c99eced243a
poruruba <44470781+poruruba@users.noreply.github.com>
2020-12-25 01:48:05 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # node-red-contrib-example-qiita-counter
+
+#概要
+
+Qiitaユーザの投稿数、Contributions、フォロワー数を取得するNode-REDのノードです。
+
+#インストール
+
+> $npm install node-red-contrib-example-qiita-counter
+
+#注意事項
+QiitaのWebページからScrapingしていますので、Webページの変更により取得できなくなる可能性があります。
+
+#依存関係
+
+- cheerio
+- node-fetch
+
+#Author
+poruruba
+
+#License
+Apache-2.0
---
+++ qiita-counter.js
@@ -0,0 +1,47 @@
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+const { URL, URLSearchParams } = require('url');
+
+module.exports = function(RED) {
+    function QiitaCounterNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', async (msg, send, done) => {
+            try{
+                const UserId = msg.payload || config.userid;
+                const text = await do_get('https://qiita.com/' + UserId, {});
+	            const $ = cheerio.load(text);
+	            const description = $("meta[name='description']").attr("content");
+                const description_list = description.split(' ');
+                if( description_list.length < 6 )
+                    throw 'get error';
+
+                msg.payload = {
+                    UserId: UserId,
+                    Items: parseInt(description_list[1]),
+                    Contributions: parseInt(description_list[3]),
+                    Followers: parseInt(description_list[5])
+                };
+                send(msg);
+            }catch(error){
+                done(error);
+            };
+        });
+    }
+    RED.nodes.registerType("qiita-counter", QiitaCounterNode);
+}
+
+function do_get(url, qs) {
+  var params = new URLSearchParams(qs);
+  var url2 = new URL(url);
+  url2.search = params;
+
+  return fetch(url2.toString(), {
+      method: 'GET',
+    })
+    .then((response) => {
+      if (!response.ok)
+        throw 'status is not 200';
+      return response.text();
+    });
+}
